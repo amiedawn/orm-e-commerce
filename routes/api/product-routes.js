e58213e6d7638a8cf86eb47eb535c79827f50c90
amiedawn <amie.d.carroll@gmail.com>
2021-01-07 13:17:28 -0700

    working on category-routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -4,13 +4,26 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  const products = await Product.findAll({
+    attributes: ['id', 'product_name', 'price', 'stock', 'category_id'],
+        include: [
+            {
+                model: Category,
+                attributes: ['id', 'category_name']
+            },
+            {
+                model: Tag,
+                attributes: ['id', 'tag_name']
+            }
+        ]
+  }
 });
 
 // get one product
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
 });
@@ -89,8 +102,22 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
+  const product = await Product.destroy({
+    where: {
+      id: req.params.id,
+    },
+    // attributes: ["id", "category_name"],
+    // include: [
+    //   {
+    //     model: Product,
+    //     attributes: ["id", "product_name", "price", "stock", "category_id"],
+    //   },
+    // ],
+  });
+  res.json(product);
+
 });
 
 module.exports = router;
